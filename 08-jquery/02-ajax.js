dab6dd869c86ac48fccf7b72e4fc058e48c19000
tin22 <tin22@pdx.edu>
2025-11-21 20:38:10 -0800

    Complete lab 8 jQuery exercises

--- 08-jquery/02-ajax.js
+++ 08-jquery/02-ajax.js
@@ -1,52 +1,51 @@
-const url = 'https://anapioficeandfire.com/api/books/';
+// Wait until the DOM is ready
+$(document).ready(function () {
+  const url = 'https://anapioficeandfire.com/api/books/';
 
-const app = document.querySelector('#books');
-app.style.paddingLeft = 0;
-const loading = document.querySelector('#loading');
+  const $app = $('#books');
+  const $loading = $('#loading');
 
-const addBookToDOM = (item) => {
-  console.log(item);
-  let element = document.createElement('div');
-  let title = document.createElement('h4');
-  let author = document.createElement('p');
-  let published = document.createElement('p');
-  let pages = document.createElement('p');
+  const addBookToDOM = (item) => {
+    // Create a card for each book
+    const $card = $('<div></div>');
 
-  element.style.display = 'flex';
-  element.style.flexDirection = 'column';
-  element.style.alignItems = 'center';
-  element.style.marginTop = '20px';
+    const $title = $('<h4></h4>').text(item.name);
+    const $author = $('<p></p>').text(`by ${item.authors.join(', ')}`);
+    const $published = $('<p></p>').text(`Published: ${item.released.substr(0, 4)}`);
+    const $pages = $('<p></p>').text(`${item.numberOfPages} pages`);
 
-  title.textContent = item.name;
-  author.textContent = `by ${item.authors[0]}`;
-  published.textContent = item.released.substr(0, 4);
-  pages.textContent = `${item.numberOfPages} pages`;
-
-  element.append(title);
-  element.append(author);
-  element.append(published);
-  element.append(pages);
-
-  app.append(element);
-};
-
-const fetchData = (url) => {
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      data.forEach((item) => {
-        addBookToDOM(item);
-      });
-    })
-    .catch((error) => {
-      console.log(error);
-      let li = document.createElement('li');
-      li.textContent = `An error occured. Please try again.`;
-      app.append(li);
-    })
-    .finally(() => {
-      app.removeChild(loading);
+    // Simple styling to center and space the cards
+    $card.css({
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      margin: '20px',
     });
-};
 
-fetchData(url);
+    $card.append($title, $author, $published, $pages);
+    $app.append($card);
+  };
+
+  const fetchData = (url) => {
+    $.ajax({
+      url: url,
+      method: 'GET',
+      dataType: 'json',
+    })
+      .done(function (data) {
+        data.forEach((item) => addBookToDOM(item));
+      })
+      .fail(function (jqXHR, textStatus, errorThrown) {
+        console.error('Error fetching books:', textStatus, errorThrown);
+        const $error = $('<p></p>').text('An error occurred. Please try again.');
+        $app.append($error);
+      })
+      .always(function () {
+        // Remove the loading gif after success or error
+        $loading.remove();
+      });
+  };
+
+  fetchData(url);
+});
+
